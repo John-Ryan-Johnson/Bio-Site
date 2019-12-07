785d3bf52964e803bcb89744cdd760fa9f24111c
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-12-07 10:30:33 -0600

    centered text in description

--- src/javascripts/components/Projects/projects.js
+++ src/javascripts/components/Projects/projects.js
@@ -3,7 +3,7 @@ import utilities from '../../helpers/utilities';
 import projectsData from '../../helpers/data/projectsData';
 
 const displayAllProjects = () => {
-  let domString = '<h2 class="text-center text-white">Projects</h2>';
+  let domString = '<h2 class="text-center text-white"></h2>';
   domString += '<div id="projects-section" class="d-flex flex-wrap justify-content-center">';
   projectsData.getAllProjects()
     .then((projects) => {
@@ -26,7 +26,7 @@ const projectCardBuilder = (project) => {
       <br>
       <h5 class="card-title text-center" id="project">${project.title}</h5>
       <p class="card-text text-center"><small class="text-muted">${project.technologiesUsed}</small></p>
-      <p class="card-text">${project.description}</p>
+      <p class="card-text text-center">${project.description}</p>
       <a class="text-center" id="projectLink" href="${project.url}">Project Link</a>
       <a class="text-center" id="projectGithubLink" href="${project.githubUrl}">Github</a>
     </div>`;
